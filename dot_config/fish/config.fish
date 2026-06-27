a1eeecaeac8ad3885106ba8ce4d12fe11e8fe586
Delvin233 <yamoahdelvin@gmail.com>
2026-06-27 03:55:28 +0000

    update: changed configs to use vim. guess ive moved to Emacs completely now lol

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,7 +5,5 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 function fish_greeting
 end
 
-set -gx EDITOR nvim
-set -gx VISUAL nvim
-set -gx NVIM_APPNAME "kickstart.nvim"
-
+set -gx EDITOR vim
+set -gx VISUAL vim
